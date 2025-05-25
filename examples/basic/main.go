54decbb7d1b752043b9b920e94aa7b83de2f7373
Johan Eliasson <johan@eliasson.me>
2025-05-25 19:02:59 +0200

    x

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -8,11 +8,7 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	exampleVar := os.Getenv("EXAMPLE_VAR")
-	exampleVarB := os.Getenv("EXAMPLE_VAR_B")
-	secretVar := os.Getenv("SECRET_VAR")
-	secretVarB := os.Getenv("SECRET_VAR_B")
-	fmt.Fprintf(w, "Hello World 5\nEXAMPLE_VAR=%s\nEXAMPLE_VAR_B=%s\nSECRET_VAR=%s\nSECRET_VAR_B=%s", exampleVar, exampleVarB, secretVar, secretVarB)
+	fmt.Fprintf(w, "Hello World")
 }
 
 func main() {
