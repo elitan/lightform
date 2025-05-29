68a9de063489809f5841068d1c5b9059fd304af3
Johan Eliasson <johan@eliasson.me>
2025-05-29 09:03:02 +0200

    x

--- docs/src/app/layout.config.tsx
+++ docs/src/app/layout.config.tsx
@@ -1,4 +1,4 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 /**
  * Shared layout configurations
@@ -12,17 +12,17 @@ export const baseOptions: BaseLayoutProps = {
     title: (
       <>
         <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
+          width='24'
+          height='24'
+          xmlns='http://www.w3.org/2000/svg'
+          aria-label='Logo'
         >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
+          <circle cx={12} cy={12} r={12} fill='currentColor' />
         </svg>
-        My App
+        Luma
       </>
     ),
   },
-  // see https://fumadocs.dev/docs/ui/navigation/links
   links: [],
+  githubUrl: "https://github.com/elitan/luma",
 };
